70cbc769988d51262633decdc4195e53650e5ae2
Arun <127246519+Arun-g03@users.noreply.github.com>
2024-07-06 04:08:40 +0100

    newbase

--- scripts.js
+++ scripts.js
@@ -104,3 +104,34 @@ function init() {
 
     animate();
 }
+
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = slides.length}
+    if (n < 1) {slideIndex = 1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
